5340fbe963a8012aff942b52f859dc87ada3f64a
CarloLaguda <carlo.laguda07@gmail.com>
2025-05-08 14:26:47 +0000

    lez 2

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
 from flask import Flask, request, redirect, render_template #render template per poter renderizare html
+from models import DatabaseWrapper #importo la classe DatabaseWrapper
+
+db = DatabaseWrapper(
+    host = "mysql-2866f0dc-iisgalvanimi-94a0.i.aivencloud.com",
+    port = 27960,
+    user = "avnadmin",
+    password = "AVNS_Gw45d55L_UtOUeZ0JSN", 
+    database = "studenti"
+)
 
 app = Flask("__name__")
-lista = [1, 2, 3, 4]
 
+"""
+lista = [1, 2, 3, 4]
+studenti = [
+    {"id": 1, "nome": "gianni"},
+    {"id": 2, "nome": "luca"}
+]
 @app.route("/")
+
 def index():
     return render_template("index.html", lista = lista) #Prende il template da templates e gli passo la lista, con nuovo nome
+"""
+@app.route("/studenti", methods = ["GET"])
+def elenco_studenti():
+    risultati = db.get_studenti()
+    return jsonify(risultati)
 
+@app.route("/studenti", methods = ["POST"])
+def aggiungi():
+    dati = request.get_json()
+    nome = dati["nome"]
+    db.aggiungi_studente(nome)
+    return({"messaggio": f"{nome} aggiunto"})
+    """
+    nome = request.form["nome"]
+    nome = {
+        "id" :len(studenti)+1,
+        "nome": nome
+    }
+    studenti.append(nome)
+    return redirect("/studenti")
+    """
+"""
+@app.route("/studenti")
+def visualizza_studente():
+    return render_template("elenco.html", studenti = studenti)
+"""
 if __name__ == "__main__":
     app.run(debug = True)
---
+++ models.py
@@ -0,0 +1,44 @@
+import pymysql
+
+class DatabaseWrapper: #utiliziamo il db come database
+    def __init__(self, host, port, user, password, database):
+        self.db_config = {
+            "host": host, 
+            "port": port,
+            "user": user,
+            "password": password,
+            "database": database,
+            "cursorclass": pymysql.cursors.DictCursor
+        }
+        self.create_table()
+    def connect(self):
+        return pymysql.connect(**self.db_config)
+    def execute_query(self, query, params=()): #Operazioni, insert, delete, create
+        conn = self.connect()
+        with conn.cursor() as cursor:
+            cursor.execute(query, params)
+            conn.commit() #Non ottengo nulla indietro
+        conn.close()
+    def fetch_query(self, query, params=()):
+        conn = self.connect()
+        with conn.cursor() as cursor:
+            cursor.execute(query, params)
+            result = cursor.fetchall()
+        conn.close()
+        return result #ritorna qualcola, select
+    
+    def create_table(self):
+        self.execute_query(
+            '''
+            CREATE TABLE IF NOT EXISTS studenti(
+                id  INT AUTO_INCREMENT PRIMARY KEY,
+                nome VARCHAR(100) NOT NULL
+            )
+            '''
+        )
+    
+    def aggiungi_studente(self, nome):
+        self.execute_query('INSERT INTO studenti (nome) VALUES (%s)', (nome,))
+    
+    def get_studenti(self):
+        return self.fetch_query('SELECT * FROM studenti')
